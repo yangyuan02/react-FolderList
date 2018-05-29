79c08ececd5013a321a6331dd0ea2f683afc9289
yangyuan <1367511704@qq.com>
2018-05-29 14:28:02 +0800

    测试2

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-//测试
+//测试222
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
